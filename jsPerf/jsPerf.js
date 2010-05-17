25a5feba3746282c280e26f21fa9ee4ca04306a3
Livingston Samuel <livingston.samuel@gmail.com>
2010-05-17 23:22:27 +0530

    jsperf.js - added parameter to override count limit

--- jsPerf/jsPerf.js
+++ jsPerf/jsPerf.js
@@ -1,21 +1,21 @@
-/* JavaScript Codeblock Performance Anlalyzer
+/* Javascript Codeblock Performance Anlalyzer
  * @author - Livingston Samuel
  */
  
 var jsPerf = {
 	getTime: function () { return (new Date()).getTime(); },
-	measure: function (name, codeBlock, iterations) {
+	measure: function (name, codeBlock, iterations, crossLimit) {
 		var count = (iterations) ? parseInt(iterations, 10) : 0,
 			fn = codeBlock,
 			startTime = jsPerf.getTime();
 		
-		if(count > 1000000 || count < 1) count = 1000;
+		if(!crossLimit && (count > 1000000 || count < 1)) count = 1000;
 		iterations = count;
 		
 		while (--count) {
 			fn();
 		}
 		
-		return name + ' - for ' + iterations + ' iterations took ' + (jsPerf.getTime() - startTime) + ' ms to complete';	
+		return name + ' - for ' + iterations + ' iterations took ' + (jsPerf.getTime() - startTime) + ' ms to execute.';
 	}
 };
