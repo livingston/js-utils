fe25af2980e6ea4ca2dc8dc1d6645e3a7b0f569d
Livingston Samuel <livingston.samuel@gmail.com>
2010-05-17 23:24:55 +0530

    jsperf.js - genarlized config options, added raw time support

--- jsPerf/jsPerf.js
+++ jsPerf/jsPerf.js
@@ -4,18 +4,19 @@
  
 var jsPerf = {
 	getTime: function () { return (new Date()).getTime(); },
-	measure: function (name, codeBlock, iterations, crossLimit) {
-		var count = (iterations) ? parseInt(iterations, 10) : 0,
+	measure: function (name, codeBlock, options) {
+		var count = (options && options.iterations) ? parseInt(options.iterations, 10) : 0,
 			fn = codeBlock,
+			iterations,
 			startTime = jsPerf.getTime();
 		
-		if(!crossLimit && (count > 1000000 || count < 1)) count = 1000;
+		if((!options || !options.crossLimit) && (count > 1000000 || count < 1)) count = 1000;
 		iterations = count;
 		
 		while (--count) {
 			fn();
 		}
 		
-		return name + ' - for ' + iterations + ' iterations took ' + (jsPerf.getTime() - startTime) + ' ms to execute.';
+		return (options && options.raw) ? (jsPerf.getTime() - startTime) : (name + ' - for ' + iterations + ' iterations took ' + (jsPerf.getTime() - startTime) + ' ms to execute.');	
 	}
 };
