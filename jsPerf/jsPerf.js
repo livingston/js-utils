f0f402685229a9cba850e37c81a328e79d4fb85c
Livingston Samuel <livingston.samuel@gmail.com>
2010-05-17 23:28:16 +0530

    jsperf.js - optimized measure fn., added compare scenarios feature & example scenario

--- jsPerf/jsPerf.js
+++ jsPerf/jsPerf.js
@@ -10,13 +10,42 @@ var jsPerf = {
 			iterations,
 			startTime = jsPerf.getTime();
 		
-		if((!options || !options.crossLimit) && (count > 1000000 || count < 1)) count = 1000;
+		if((!options || !options.crossLimit) && (count > 1000000 || count < 1)) count = 10;
 		iterations = count;
 		
-		while (--count) {
+		while (count--) {
 			fn();
 		}
 		
 		return (options && options.raw) ? (jsPerf.getTime() - startTime) : (name + ' - for ' + iterations + ' iterations took ' + (jsPerf.getTime() - startTime) + ' ms to execute.');	
+	},
+	compare: function (scenarios) {
+		var scenario, func, options = scenarios.options || {};
+
+		for ( scenario in scenarios ) {
+			if(scenarios.hasOwnProperty(scenario)) {
+				scenario = scenarios[scenario].fn;
+
+				for(func in scenario) {
+					if(scenario.hasOwnProperty(func)) {
+						console.log(jsPerf.measure(func, scenario[func], options));
+					}
+				}
+			}
+		}
 	}
 };
+
+var scenarios = {
+	"select all elements": {
+		fn: {
+			'qsa': function () { document.querySelectorAll('*'); },
+			'gebtn': function () { document.getElementsByTagName('*'); }
+		},
+		options: {
+			iterations: 10000
+		}
+	}
+};
+
+jsPerf.compare(scenarios);
