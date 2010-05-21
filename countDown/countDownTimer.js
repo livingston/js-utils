210cbb54a493c4c62c9b3a09f4b8ec65d9d716b4
Livingston Samuel <livingston.samuel@gmail.com>
2010-05-22 02:12:44 +0530

    writing countDownTimer from scratch, implemented efficient count down timer

---
+++ countDown/countDownTimer.js
@@ -0,0 +1,56 @@
+/*! countDown Timer
+ *
+ * @version 2
+ * @author Livingston Samuel
+ * @license MIT License
+ *
+ */
+
+(function (win, doc, Date) {
+  var countDownTimer = function () {
+    var displayElem = doc.createElement('div'),
+        pad = function (n) {
+          var l;
+          return (n = '0' + n, l = n.length, n.substr(l-2, l));
+        },
+        formatTime = function (rawTime) {
+          if (rawTime < 1000) { return '00 : 00 : 00'; }
+          var inSecs = rawTime / 1000,
+              inMins = Math.floor(inSecs / 60),
+              inHrs = Math.floor(inMins / 60);
+
+          inSecs = inSecs % 60;
+          inMins = inMins % 60;
+
+          return pad(inHrs) + " : " + pad(Math.floor(inMins)) + " : " + pad(Math.floor(inSecs));
+        }, timer,
+        time = {},
+        processTime = function () {
+          time.elasped = (+new Date()) - time.start;
+          if (time.elasped < time.limit) {
+            setTimeout(arguments.callee, 500)
+          }
+          update(formatTime(time.limit - time.elasped));
+        }, update = function (time) {
+          displayElem.innerHTML = time;
+        };
+
+    doc.getElementsByTagName('body')[0].appendChild(displayElem);
+    
+    return {
+      time: function () {},
+      start: function (limit) {
+        time.start = +new Date();
+        time.limit = limit;
+        
+        processTime();
+      },
+      pause: function () {},
+      stop: function () {},
+      reset: function () {}
+    }
+  }();
+  
+  countDownTimer.start(1*60*1000);
+  
+}(window, document, Date));
